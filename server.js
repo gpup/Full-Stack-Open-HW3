0acfaf73c7488b8107984825b241893ed183a326
Seth Martin <jahgpup@gmail.com>
2022-06-15 13:52:26 -0600

    exercises from the end of full stack open 3

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const PORT = 3001
+
+app.use(express.json())
 
 let persons = [
     { 
@@ -26,4 +29,69 @@ let persons = [
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+})
+
+app.get('/info', (req, res) => {
+    const currentDate = new Date()
+    res.send(`<h2>Phonebook has info for ${persons.length} people</h2> <h2>${currentDate}</h2>`)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id
+    const entry = persons.find(entry => entry.id == id)
+    
+    if(entry){
+        res.json(entry)
+    }else{
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons.filter(entry => entry.id !== id)
+
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+    
+    if(!body.name){
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    if(!body.number){
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    if(persons.some(entry => entry.name === body.name)){
+        return res.status(409).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const entry = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(entry)
+    res.json(entry)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server active on ${PORT}`)
 })
